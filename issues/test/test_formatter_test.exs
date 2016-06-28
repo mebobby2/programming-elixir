9a119a819b3d4c491122ca5749e4f083c154e416
Bobby Lei <bobl53@gmail.com>
2016-06-28 22:32:49 +0800

    fixing tests

--- issues/test/test_formatter_test.exs
+++ issues/test/test_formatter_test.exs
@@ -40,8 +40,9 @@ defmodule TableFormatterTest do
     end
     assert result == """
     c1    | c2     | c4     
+    ------+--------+--------
     r1 c1 | r1 c2  | r1+++c4
-    r2 c1 | r1 c2  | r2 c4  
+    r2 c1 | r2 c2  | r2 c4  
     r3 c1 | r3 c2  | r3 c4  
     r4 c1 | r4++c2 | r4 c4  
     """
